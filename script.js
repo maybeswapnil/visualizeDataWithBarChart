aa51cf56971d8b52ce3d07c23a1674bc35bb7a83
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2020-09-30 18:15:32 +0530

    final

--- script.js
+++ script.js
@@ -23,16 +23,27 @@ req.send();
 
 function main() {
 
-        console.log(data);
+        console.log(dates);
 
         const xScale = d3.scaleBand()
                         .domain(values.map((d) => d[0]))
                         .rangeRound([0, 1500])
                         .padding(0.1);
 
+        const scale = d3.scaleLinear()
+                        .domain(dates.map((d) => d))
+                        .range([0, 1500]);
+        
+        var xMax = new Date(d3.max(dates));
+        xMax.setMonth(xMax.getMonth() + 3);
+
+        var cale = d3.scaleTime()
+                     .domain([d3.min(dates), xMax])
+                     .range([0, 1400]);
+
         const yScale = d3.scaleLinear()
-                        .domain([20, 1820])
-                        .range([1820, 0]); 
+                        .domain([0, 21000])
+                        .range([650, 0]); 
 
         const container = d3.select("svg")
                         .classed("container", true);
@@ -43,8 +54,37 @@ function main() {
                             .enter()
                             .append('rect')
                             .classed("bar", true)
+                            .attr('data-date',  (d) => (d[0]))
+                            .attr('data-gdp',(d) => (d[1]))
                             .attr("width", 3)
-                            .attr("height", (d) =>1900 - yScale(d[1]/10))
+                            .attr("height", (d) => 650 - yScale(d[1]))
                             .attr("x", (d) => xScale(d[0]))
-                            .attr("y", (d) => yScale(d[1]/10));
+                            .attr("y", (d) => yScale(d[1]))
+                            .append('title')
+                            .attr("id", "tooltip")
+                            .text((d) => (d[0]) + " :: $" + (d[1]) + " Billion");
+
+            container.selectAll("text")
+                            .data(dataset)
+                            .enter()
+                            .append("text")
+                            .text((d) => d[1])
+                            .attr("x", (d, i) => i * 30)
+                            .attr("y", (d, i) => h - (d * 3 + 3))
+
+        const xAxis = d3.axisBottom()
+                        .scale(cale);
+        
+        container.append("g")
+           .attr("id", "x-axis")
+           .attr("transform", "translate(50, 660)")
+           .call(xAxis);
+
+        const yAxis = d3.axisLeft()
+                        .scale(yScale);
+
+        container.append("g")
+            .attr("id", "y-axis")
+            .attr("transform", "translate(50, 0)")
+            .call(yAxis);
     }
